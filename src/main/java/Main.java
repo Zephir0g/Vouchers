dd682948b27083c1d1f52d152d68c4d1734311c1
Vlad <zinkovskyi.vladyslav@gmail.com>
2023-02-01 06:33:59 +0100

    create constructor and createVoucher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import vouchers.ControlVoucher;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        ControlVoucher voucher  = new ControlVoucher();
+        voucher.createVoucher(10, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+        voucher.createVoucher(10, "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 5);
+
+
     }
 }
--- src/main/java/vouchers/Voucher.java
+++ src/main/java/vouchers/Voucher.java
@@ -1,4 +1,36 @@
 package vouchers;
 
 public class Voucher {
+
+    String name, description;
+    int discount;
+    boolean access;
+
+    public Voucher(String name, String description, int discount, boolean access) {
+        this.name = name;
+        this.description = description;
+        this.discount = discount;
+        this.access = access;
+    }
+
+    public Voucher(String name, String description, int discount) {
+        this.name = name;
+        this.description = description;
+        this.discount = discount;
+    }
+
+    public Voucher(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Voucher(String name) {
+        this.name = name;
+    }
+
+    public Voucher() {
+    }
+
+
+
 }
